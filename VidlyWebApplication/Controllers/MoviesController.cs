52b0546dd152d9f184d05589184c026740941e0e
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-16 06:01:15 -0400

    Demo of different types of ActionResults

--- VidlyWebApplication/Controllers/MoviesController.cs
+++ VidlyWebApplication/Controllers/MoviesController.cs
@@ -13,7 +13,14 @@ namespace VidlyWebApplication.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+
+            // ActionResults which are the output of our Actions
+
             return View(movie);
+            // return Content("Hello World");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
