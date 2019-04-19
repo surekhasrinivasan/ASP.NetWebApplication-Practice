94d224b155bd7f93a8bd20dcd66dc3e27cd38b36
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-18 21:06:32 -0400

    Created a ViewModels folder to keep RandomMovieViewModel.cs

---
+++ VidlyWebApplication/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlyWebApplication.Models;
+
+namespace VidlyWebApplication.ViewModels
+{
+    // ViewModel is a Model specifically built for a View. It includes any data and rules specific to that view. 
+
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
