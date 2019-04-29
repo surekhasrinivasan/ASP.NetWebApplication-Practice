640f45cd83744a22f37d84e67684c232af5f3a73
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-28 22:28:09 -0400

    Created  a ViewResult and ActionResult to GetCustomers and GetCustomer by Id

--- VidlyWebApplication/Controllers/CustomersController.cs
+++ VidlyWebApplication/Controllers/CustomersController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VidlyWebApplication.Models;
 
 namespace VidlyWebApplication.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var customers = GetCustomers();
+            return View(customers);
         }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        
+
     }
 }
