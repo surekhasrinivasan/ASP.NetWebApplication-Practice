e8e8ecd5aff469be81cf72fb97f2a6420adacbcd
Surekhasrinivasan <surekhasrao@gmail.com>
2019-04-28 22:28:53 -0400

    Implemented GetCustomers method

--- VidlyWebApplication/Controllers/CustomersController.cs
+++ VidlyWebApplication/Controllers/CustomersController.cs
@@ -24,8 +24,15 @@ namespace VidlyWebApplication.Controllers
 
             return View(customer);
         }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith"},
+                new Customer { Id = 2, Name = "Mary Williams"}
+            };
+        }
 
-        
 
     }
 }
